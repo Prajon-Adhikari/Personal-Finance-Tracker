1859e82d0eacc35ae09a401029646c818faf42f2
Prajon-Adhikari <prajonadhikari44@gmail.com>
2025-01-13 14:30:19 +0545

    fetching data for pie chart and table

--- frontend/src/Components/Reports.jsx
+++ frontend/src/Components/Reports.jsx
@@ -11,6 +11,8 @@ export default function Reports() {
   const [totalIncome, setTotalIncome] = useState(0);
   const [totalExpense, setTotalExpense] = useState(0);
   const [highestExpenseCategory, setHighestExpenseCategory] = useState("");
+  const [highestIncomeWeek, setHighestIncomeWeek] = useState("");
+  const [highestExpenseWeek, setHighestExpenseWeek] = useState("");
 
   const [barChartData, setBarChartData] = useState({
     series: [],
@@ -41,10 +43,48 @@ export default function Reports() {
       tooltip: { enabled: true },
     },
   });
+  const [pieChartData, setPieChartData] = useState({
+    series: [],
+    options: {
+      chart: {
+        type: "pie",
+      },
+      colors: [
+        "#FF9100",
+        "#914F1E",
+        "#000957",
+        "#B03052",
+        "#F95454",
+        "#000000",
+      ], // Custom colors
+      labels: [
+        "Food",
+        "Shopping",
+        "Electricity",
+        "Entertainment",
+        "Health",
+        "Others",
+      ],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200,
+            },
+            legend: {
+              position: "bottom",
+            },
+          },
+        },
+      ],
+    },
+  });
 
   useEffect(() => {
     fetchBarGraphData(currentMonth);
     fetchAreaChartData(currentMonth);
+    fetchPieChartData(currentMonth);
     console.log(currentMonth);
   }, [currentMonth]);
 
@@ -76,6 +116,7 @@ export default function Reports() {
           expenseData[weekIndex] += amount; // Accumulate expenses
         }
       });
+
       const totalIncomeCalculated = incomeData.reduce(
         (sum, week) => sum + week,
         0
@@ -87,6 +128,26 @@ export default function Reports() {
 
       setTotalIncome(totalIncomeCalculated);
       setTotalExpense(totalExpenseCalculated);
+
+      let incomeWeek = incomeData[0];
+      let expenseWeek = expenseData[0];
+
+      for (let i = 0; i < incomeData.length; i++) {
+        const weekTotal = incomeData[i];
+        if (weekTotal > incomeWeek) {
+          incomeWeek = weekTotal;
+        }
+      }
+
+      for (let i = 0; i < expenseData.length; i++) {
+        const weekTotal = expenseData[i];
+        if (weekTotal > expenseWeek) {
+          expenseWeek = weekTotal;
+        }
+      }
+
+      setHighestIncomeWeek(incomeWeek);
+      setHighestExpenseWeek(expenseWeek);
 
       setBarChartData({
         series: [
@@ -151,7 +212,6 @@ export default function Reports() {
       );
       const data = await response.json();
 
-      const categories = ["Week 1", "Week 2", "Week 3", "Week 4"];
       const FoodData = [0, 0, 0, 0];
       const ShoppingData = [0, 0, 0, 0];
       const SalesData = [0, 0, 0, 0];
@@ -169,21 +229,21 @@ export default function Reports() {
 
         const amount = parseFloat(item.amount);
         if (item.category === "Food") {
-          FoodData[weekIndex] += amount; // Accumulate income
+          FoodData[weekIndex] += amount;
         } else if (item.category === "Shopping") {
-          ShoppingData[weekIndex] += amount; // Accumulate expenses
+          ShoppingData[weekIndex] += amount;
         } else if (item.category === "Sales") {
-          SalesData[weekIndex] += amount; // Accumulate expenses
+          SalesData[weekIndex] += amount;
         } else if (item.category === "Entertainment") {
-          EntertainmentData[weekIndex] += amount; // Accumulate expenses
+          EntertainmentData[weekIndex] += amount;
         } else if (item.category === "Health") {
-          HealthData[weekIndex] += amount; // Accumulate expenses
+          HealthData[weekIndex] += amount;
         } else if (item.category === "Other") {
-          OtherData[weekIndex] += amount; // Accumulate expenses
+          OtherData[weekIndex] += amount;
         } else if (item.category === "Electricity") {
-          ElectricityData[weekIndex] += amount; // Accumulate expenses
+          ElectricityData[weekIndex] += amount;
         } else if (item.category === "Salary") {
-          SalaryData[weekIndex] += amount; // Accumulate expenses
+          SalaryData[weekIndex] += amount;
         }
       });
 
@@ -198,6 +258,31 @@ export default function Reports() {
         { name: "Others", data: OtherData },
       ];
 
+      const expenseSeries = [
+        { name: "Food", data: FoodData },
+        { name: "Shopping", data: ShoppingData },
+        { name: "Entertainment", data: EntertainmentData },
+        { name: "Health", data: HealthData },
+        { name: "Electricity", data: ElectricityData },
+        { name: "Others", data: OtherData },
+      ];
+
+      const caluclateTotal = (array) =>
+        array.reduce((sum, value) => sum + value, 0);
+
+      let highestData = expenseSeries[0];
+      let highestTotal = caluclateTotal(highestData.data);
+
+      for (let i = 0; i < expenseSeries.length; i++) {
+        const total = caluclateTotal(expenseSeries[i].data);
+        if (total > highestTotal) {
+          highestData = expenseSeries[i];
+          highestTotal = total;
+        }
+      }
+
+      setHighestExpenseCategory(highestData.name);
+
       setAreaChartData((prevState) => ({
         ...prevState,
         series: series,
@@ -207,43 +292,54 @@ export default function Reports() {
     }
   };
 
-  const [pieChartState, setPieChartState] = useState({
-    series: [70, 55, 24, 30, 25, 15],
-    options: {
-      chart: {
-        type: "pie",
-      },
-      colors: [
-        "#D2649A",
-        "#FF9100",
-        "#914F1E",
-        "#000957",
-        "#B03052",
-        "#F95454",
-      ], // Custom colors
-      labels: [
-        "Food",
-        "Shopping",
-        "Electricity",
-        "Entertainment",
-        "Health",
-        "Others",
-      ],
-      responsive: [
-        {
-          breakpoint: 480,
-          options: {
-            chart: {
-              width: 200,
-            },
-            legend: {
-              position: "bottom",
-            },
-          },
-        },
-      ],
-    },
-  });
+  const fetchPieChartData = async (yearMonth) => {
+    try {
+      const response = await fetch(
+        `http://localhost:8000/menu/reports/${yearMonth}`
+      );
+      const data = await response.json();
+
+      let FoodData = 0;
+      let ShoppingData = 0;
+      let EntertainmentData = 0;
+      let HealthData = 0;
+      let ElectricityData = 0;
+      let OtherData = 0;
+
+      data.forEach((item) => {
+        const amount = parseFloat(item.amount);
+        if (item.category === "Food") {
+          FoodData += amount;
+        } else if (item.category === "Shopping") {
+          ShoppingData += amount;
+        } else if (item.category === "Entertainment") {
+          EntertainmentData += amount;
+        } else if (item.category === "Health") {
+          HealthData += amount;
+        } else if (item.category === "Other") {
+          OtherData += amount;
+        } else if (item.category === "Electricity") {
+          ElectricityData += amount;
+        }
+      });
+
+      const expenseSeries = [
+        FoodData,
+        ShoppingData,
+        EntertainmentData,
+        HealthData,
+        ElectricityData,
+        OtherData,
+      ];
+
+      setPieChartData((prevState) => ({
+        ...prevState,
+        series: expenseSeries,
+      }));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const lineChartState = {
     series: [
@@ -314,7 +410,7 @@ export default function Reports() {
           </button>
         </div>
         <div className="mt-12 flex justify-between">
-          <div className="w-[450px] p-6 rounded-md shadow-[0px_0px_5px] border-slate-300">
+          <div className="w-[420px] p-6 rounded-md shadow-[0px_0px_5px] border-slate-300">
             <div className="flex justify-between items-center">
               <div className="font-bold text-3xl">Monthly Analysis</div>
               <div className="font-medium text-lg">{currentMonth}</div>
@@ -327,11 +423,14 @@ export default function Reports() {
             <div className="py-4 font-medium text-lg border-b-2">
               Total Expenses : ${totalExpense}
             </div>
-            <div className="py-4 font-medium text-lg"> Net Balance : $1000</div>
+            <div className="py-4 font-medium text-lg">
+              {" "}
+              Net Balance : ${totalIncome - totalExpense}
+            </div>
             <div className="py-4 font-medium text-lg">
               Highest Spending Category :
-              <span className="px-3 py-1 bg-red-500 rounded-lg text-white ml-8">
-                Food
+              <span className="px-3 py-2 bg-red-500 rounded-lg text-white ml-8">
+                {highestExpenseCategory}
               </span>
             </div>
           </div>
@@ -340,20 +439,20 @@ export default function Reports() {
               <div>
                 <div className="font-semibold text-xl">
                   {" "}
-                  Highest Gained Week
+                  Highest Gained in a Week
                 </div>
-                <div className=" text-lg">Week 3</div>
+                <div className=" text-lg">${highestIncomeWeek}</div>
               </div>
               <div>
                 <div className="font-semibold text-xl">
                   {" "}
-                  Highest Expensed Week
+                  Highest Expensed in a Week
                 </div>
-                <div className=" text-lg">Week 3</div>
+                <div className=" text-lg">${highestExpenseWeek}</div>
               </div>
               <div>
                 <div className="font-semibold text-xl"> Net Balance</div>
-                <div className=" text-lg">$1000</div>
+                <div className=" text-lg"> ${totalIncome - totalExpense}</div>
               </div>
             </div>
             <div className="w-[320px]">
@@ -379,8 +478,8 @@ export default function Reports() {
               <div className=" rounded-lg p-4 border-slate-200">
                 <div className="pb-4 font-bold text-2xl">Expenses</div>
                 <ReactApexChart
-                  options={pieChartState.options}
-                  series={pieChartState.series}
+                  options={pieChartData.options}
+                  series={pieChartData.series}
                   type="pie"
                   width={400}
                 />
@@ -398,14 +497,14 @@ export default function Reports() {
                     </tr>
                   </thead>
                   <tbody>
-                    {pieChartState.options.labels.map((label, index) => {
+                    {pieChartData.options.labels.map((label, index) => {
                       // Calculate percentage for each category
-                      const total = pieChartState.series.reduce(
+                      const total = pieChartData.series.reduce(
                         (sum, value) => sum + value,
                         0
                       );
                       const percentage = (
-                        (pieChartState.series[index] / total) *
+                        (pieChartData.series[index] / total) *
                         100
                       ).toFixed(2);
 
